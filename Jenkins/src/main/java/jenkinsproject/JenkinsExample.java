924fff327aea42b5fcc59e6a65cf0fc30a2d583c
arunima1996 <167090136+arunima1996@users.noreply.github.com>
2024-05-12 17:46:07 +0400

    Sample commit

---
+++ Jenkins/src/main/java/jenkinsproject/JenkinsExample.java
@@ -0,0 +1,12 @@
+package jenkinsproject;
+
+public class JenkinsExample {
+
+	public static void main(String[] args) 
+	{
+
+         System.out.println("This is an example for Jenkins");
+
+	}
+
+}
